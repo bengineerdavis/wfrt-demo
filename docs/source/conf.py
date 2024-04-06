be1d4bf8c433e9b0d964e680a29500768ac36aba
Ben Davis <bengineerdavis@gmail.com>
2024-04-05 20:26:30 -0700

    updates theme to 'sphinx_book_theme'

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,23 +6,22 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Sphinx Quickstart Tutorial'
-copyright = '2024, Ben Davis'
-author = 'Ben Davis'
-release = '1.0.0'
+project = "Sphinx Quickstart Tutorial"
+copyright = "2024, Ben Davis"
+author = "Ben Davis"
+release = "1.0.0"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = []
 
-templates_path = ['_templates']
-exclude_patterns = []
-
+templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "sphinx_book_theme"
+html_static_path = ["_static"]
