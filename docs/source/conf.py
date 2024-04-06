0dd4a188aed287d84a688a9bf1190cf3d5002549
Ben Davis <bengineerdavis@gmail.com>
2024-04-05 20:53:22 -0700

    added api to docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,6 +3,11 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
@@ -14,7 +19,16 @@ release = "1.0.0"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    # "sphinx.ext.mathjax",
+    "sphinx.ext.autodoc",
+    # "sphinx.ext.coverage",
+    "sphinx.ext.napoleon",
+    # "sphinx.ext.autosectionlabel",
+    # "nbsphinx",
+    # "myst_parser",
+    # "sphinx_copybutton",
+]
 
 templates_path = ["_templates"]
 exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
